c4833e1055310edefb0efcba311d0ad804d8dea6
TylerDurand <tylerdurand14@gmail.com>
2024-11-18 14:17:10 -0500

    improved upon uploadevent and event loading
    installed multer using npm for reading blob files
    
    added to home.html , event.js , and event.html so that at least jpeg images can be uploaded to the database through the form
    
    added event_cost and user_name to the home.html form, event.js, and event.html.
    
    made minor changes to app.js and event.js,namely removed console.log() used for testing

--- routes/event.js
+++ routes/event.js
@@ -1,29 +1,58 @@
 //establish event route
 const { Router } = require('express');
 const db = require('../database');
+const multer = require('multer');
 const router = Router();
-
-
-//get request test
-router.get('/', (req,res,next) => {
-  console.log('Request made to /EVENT ROUTE');
-  next();
-});
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
 // get all events
 router.get('/', async (req,res) => {
-  const results = await db.promise().query(`SELECT * FROM Event`);
-  console.log(results);
-    res.status(200).send(results[0]);
+  try {
+    const [results] = await db.promise().query(`SELECT * FROM Event`);
+    const events = results.map(event => {
+      if (event.event_img) {
+          event.event_img = Buffer.from(event.event_img).toString('base64');
+      } else {
+          event.event_img = null;
+      }
+      return event;
+  });
+    res.status(200).json(events);
+} catch (err) {
+    console.log(err);
+    res.status(500).send('Error fetching events');
+}
   });
 
 //upload event
-router.post('/uploadEvent', async (req,res) => {
+router.post('/uploadEvent', upload.single('event_img'), async (req,res) => {
   const user_name = await req.user;
-  const {event_name, start_time, end_time, event_description, event_location, event_img} = req.body;
+  let {event_name, start_time, end_time, event_description, event_cost, event_location} = req.body;
+  if (event_cost === ''){
+    event_cost = '0.00';
+  }
+  
+  let event_img = null;
+  if (req.file) {
+    event_img = req.file.buffer;
+  }
   if (event_name && start_time && end_time && event_location){
     try {
-    db.promise().query(`INSERT INTO Event (user_name, event_name, start_time, end_time, event_description, event_location, event_img) VALUES('${user_name[0].user_name}', '${event_name}', '${start_time}', '${end_time}', '${event_description}', '${event_location}', '${event_img}')`);
+      await db.promise().query(
+        `INSERT INTO Event (user_name, event_name, start_time, end_time, event_description, event_cost, event_location, event_img) 
+         VALUES (?, ?, ?, ?, ?, ?, ?, ?)`,
+        [
+            user_name[0].user_name,
+            event_name,
+            start_time,
+            end_time,
+            event_description,
+            event_cost,
+            event_location,
+            event_img
+        ]
+    );
     res.status(201).redirect('/public/Event.html');
     }
     catch (err) {
